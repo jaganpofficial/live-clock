d60c0fef89b52b8077833c0565c1ff85016f1515
Jagan <jaganjnofficial@gmail.com>
2023-12-02 18:07:29 +0530

    clock

---
+++ script.js
@@ -0,0 +1,16 @@
+let second = 0;
+let minute = 0;
+let hour = 0;
+let date = new Date();
+
+setInterval(
+    function(){
+        date = new Date();
+        second = date.getSeconds() * 6;
+        minute = date.getMinutes() * 6;
+        hour = date.getHours() * 30 + Math.round(minute / 12);
+        document.getElementById("second-hand").style.transform = "rotate("+ second + "deg)";
+        document.getElementById("minute-hand").style.transform = "rotate("+ minute + "deg)";
+        document.getElementById("hour-hand").style.transform = "rotate("+ hour + "deg)";
+    },1000
+);
